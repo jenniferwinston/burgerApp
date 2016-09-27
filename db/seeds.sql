084e714eef5e9b3b822a783698a48a993e629c53
Jennifer Winston <winston.jennifer9@gmail.com>
2016-09-26 21:19:45 -0400

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain Hamburger on Kaiser Roll');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Grilled Cheese Burger');

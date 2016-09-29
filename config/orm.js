77566c3c04f83ac2ebc768401e860e87a99cb9cc
Jennifer Winston <winston.jennifer9@gmail.com>
2016-09-29 15:12:09 -0400

    orms

--- config/orm.js
+++ config/orm.js
@@ -4,29 +4,76 @@
 
 var connection = require("../config/connection.js");
 
+	function printQuestionMarks(num){
+		var arr =[];
+			for (var i =0; i < num; i++){
+				arr.push('?');
+			};
+			return arr.toString();
+	};
+
+	function objToSql(ob){
+		var arr=[];
+		for (var key in ob){
+			arr.push(key + '=' + ob[key]);
+		};
+		return arr.toString();
+	};
+
 var orm = {
 
-	selectAll: function(){
+	all: function(tableInput, cb){
 
-			var queryString = ;
-			connection.query( function(){
+			var queryString = 'SELECT * FROM ' + tableInput + ';';
+			connection.query(queryString, function(err, result) {
+				if (err) throw err;	
+				cb(result);
+			});
+	},
+	// vals is an array of values that we want to save to cols
+	// cols are the columns we want to insert the values into
+	create: function(table, cols, vals, cb){
+			var queryString = 'INSERT INTO ' + table ;
+			queryString = queryString + ' (';
+			queryString = queryString + cols.toString();
+			queryString = queryString + ') ';
+			queryString = queryString + 'VALUES (';
+			queryString = queryString + printQuestionMarks(vals.length);
+			queryString = queryString + ') ';
+			console.log(queryString);
 
-		});
+			connection.query(queryString, vals, function(err, result){
+				if (err) throw err;
+				cd(result);
+			});
 	},
 
-	insertOne: function(){
-			var queryString = ;
-			connection.query( function(){
+	// objColVals would be the columns and values that you want to update
+	update: function(table, objColVals, condition, cb){
+			var queryString = 'UPDATE' + table;
+			queryString = queryString + ' SET ';
+			queryString = queryString + objToSql(objColVals);
+			queryString = queryString + 'WHERE';
+			queryString = queryString + condition;
+			console.log(queryString);
 
-		});
+			connection.query(queryString, function(err, result){
+				if (err) throw err;
+				cb(result);
+			});
 	},
 
-	updateOne: function(){
-			var queryString = ;
-			connection.query( function(){
+	delete: function(table, condition, cb){
+		var queryString= 'DELETE FROM ' + table;
+		queryString = queryString + 'WHERE ';
+		queryString = queryString + condition;
 
+		connection.query(queryString, function(err, result){
+			if (err) throw err;
+			cb(result);
 		});
 	}
 };
 
 module.exports = orm;
+
--- server.js
+++ server.js
@@ -10,11 +10,12 @@ var routes = require('./controllers/burgers_controller.js');
 
 
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(process.cwd() + '/public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
